015a4111742d1618c1952dbe95d1b9aadd9d8024
Anca N <negulescuanca11@gmail.com>
2022-12-28 14:03:27 +0100

    WIP write real values to Google Sheet

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 import re
 import time
 from datetime import date
+from write_to_google_sheets import main as gs_write
 
 import os
 from dotenv import load_dotenv
+
+def get_first_int_from_string(string):
+	return int((re.findall(r'\d+', string))[0])
 
 def main():
 	print("Hoi!")
@@ -21,7 +25,7 @@ def main():
 	driver.get(url)
 		
 	results_element_text = (driver.find_element("xpath", '//html/body/div/main/div/div[2]/div[1]/div/div/div[1]/h1')).text
-	results_count = int((re.findall(r'\d+', results_element_text))[0])
+	results_count = get_first_int_from_string(results_element_text)
 
 	date_today = date.today().strftime("%d.%m.%Y")
 
@@ -41,20 +45,24 @@ def main():
 			bedrooms = driver.find_element("xpath", f'//html/body/div/main/div/div[2]/div[2]/div[2]/div/ul/li[{i}]/article/div[1]/div[1]/div[2]/div/ul/li[2]/b').text
 			is_apartment = (driver.find_element("xpath", f'//html/body/div/main/div/div[2]/div[2]/div[2]/div/ul/li[{i}]/article/div[1]/div[1]/div[1]/div/p[2]').text).lower() == 'apartment'
 			sqm_element_text = driver.find_element("xpath", f'//html/body/div/main/div/div[2]/div[2]/div[2]/div/ul/li[{i}]/article/div[1]/div[1]/div[2]/div/ul/li[3]/b').text
-			sqm = int((re.findall(r'\d+', sqm_element_text))[0])
-			price = driver.find_element("xpath", f'//html/body/div/main/div/div[2]/div[2]/div[2]/div/ul/li[{i}]/article/div[1]/div[1]/div[2]/div/ul/li[1]/b').text
+			sqm = get_first_int_from_string(sqm_element_text)
+			price_element_text = driver.find_element("xpath", f'//html/body/div/main/div/div[2]/div[2]/div[2]/div/ul/li[{i}]/article/div[1]/div[1]/div[2]/div/ul/li[1]/b').text
+			price_value = get_first_int_from_string(price_element_text)
 			# availability = driver.find_element("xpath", f'//html/body/div/main/div/div[2]/div[2]/div[2]/div/ul/li[{i}]/article/div[2]/span').text
 		except:
 			continue
 
 		# TODO: Filter by availability.
 		if(is_apartment):
-			print(f"-----Apartment {i} - {price} ")
+			print(f"-----Apartment {i} - {price_element_text} ")
 			print(address)
 			print(f"{sqm} sqm")
 			print(f"{bedrooms} bedrooms---")
 
 			print("")
+
+			values = [[address, sqm, bedrooms, price_value]]
+			gs_write(values)
 
 	print("Tot ziens!")
 
--- write_to_google_sheets.py
+++ write_to_google_sheets.py
@@ -5,29 +5,17 @@ from dotenv import load_dotenv
 
 from connect_to_google_sheets import main as gs_connect
 
-def main():
+def main(values):
 	load_dotenv()
 
 	SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
-	RANGE_NAME = 'Data!A2:N'
+	RANGE_NAME = 'Data!B2:N'
 
 	try:
 		service = gs_connect()
 
 		if (service is not False):
 			# Call the Sheets API
-			values = [
-				[
-					'Example like', 'Example address', 'Example size', 'Example floor', 'Example year', 
-					'Example start', 'Example rent', 'Example service costs', '', 'Example utilities cost', 
-					'Example total costs', 'Example total pp', 'Example gross pm one', 
-				],
-				[
-					'Example like2', 'Example address2', 'Example size2', 'Example floor2', 'Example year2', 
-					'Example start2', 'Example rent2', 'Example service costs2', '', 'Example utilities cost2', 
-					'Example total costs2', 'Example total pp2', 'Example gross pm one2', 
-				],
-			]
 			body = {
 				'values': values
 			}
