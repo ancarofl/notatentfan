da8c09df9428b25b8818d75dcf7f8980260245bc
Anca N <negulescuanca11@gmail.com>
2022-12-28 12:44:21 +0100

    add constants file; remove unused Google Sheets API sample script; remove debug prints

--- connect_to_google_sheets.py
+++ connect_to_google_sheets.py
@@ -6,28 +6,25 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+import constants
+
 def main():
 	"""Connect to Google Sheets API. 
 	Returns resource to interact with the API on success. Otherwise returns None."""
-	print("Connecting to Google Sheets API...") # TODO: Remove debug
-
-	# Available scopes: https://developers.google.com/identity/protocols/oauth2/scopes#sheets.
-	# Delete token.json if modifying scopes.
-	SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 	credentials = None
 	
 	# Token.json stores the user's access and refresh tokens. 
 	# It is created automatically when the authorization flow completes for the first time.
 	if os.path.exists('token.json'):
-		credentials = Credentials.from_authorized_user_file('token.json', SCOPES)
+		credentials = Credentials.from_authorized_user_file('token.json', constants.FULL_ACCESS_SCOPE)
 	
 	# If there are no (valid) credentials available, ask the user to log in.
 	if not credentials or not credentials.valid:
 		if credentials and credentials.expired and credentials.refresh_token:
 			credentials.refresh(Request())
 		else:
-			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', constants.FULL_ACCESS_SCOPE)
 			credentials = flow.run_local_server(port=0)
 		# Save the credentials
 		with open('token.json', 'w') as token:
@@ -35,10 +32,8 @@ def main():
 
 	try:
 		service = build('sheets', 'v4', credentials=credentials)
-		print("Succesfully connect to Google Sheets API.") # TODO: Remove debug
 		return service
 	except:
-		print("An error occured when trying to connect to Google Sheets API.") # TODO: Remove debug
 		return None
 
 if __name__ == '__main__':
---
+++ constants.py
@@ -0,0 +1,2 @@
+READONLY_SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly' # See all your Google Sheets
+FULL_ACCESS_SCOPE = 'https://www.googleapis.com/auth/spreadsheets' # See, edit, create & delete all your Google Sheets
